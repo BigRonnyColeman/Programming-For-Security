4f23adb8f66a1de4e9cc2651670c2cb68ea59572
BigRonnyColeman <71296085+BigRonnyColeman@users.noreply.github.com>
2022-05-30 22:30:24 +1000

    Fresh Start

---
+++ db.js
@@ -0,0 +1,14 @@
+const Mongoose = require("mongoose");
+
+const localDB = `mongodb+srv://admin:admin@pfs.2q42h.mongodb.net`;
+
+const connectDB = async () => {
+  await Mongoose.connect(localDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  console.log("MongoDB Connected");
+};
+
+module.exports = connectDB;
