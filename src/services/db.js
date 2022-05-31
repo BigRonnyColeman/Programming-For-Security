ad67bc00a1289e716490dd2392bc9e3fd7c37dd1
jacquim5678 <jacquim5678@gmail.com>
2022-05-31 22:18:49 +1000

    some chnages to database connection, plus start of ts->js conversion

--- src/services/db.js
+++ src/services/db.js
@@ -1,12 +1,14 @@
 const Mongoose = require("mongoose");
 
-const localDB = `mongodb+srv://admin:admin@pfs.2q42h.mongodb.net`;
+const localDB = `mongodb+srv://admin:admin@pfs.2q42h.mongodb.net/pfs`;
+
 
 const connectDB = async () => {
   await Mongoose.connect(localDB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
+
 
   console.log("MongoDB Connected");
 };
